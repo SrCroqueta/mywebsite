da949c2c538cf01c61a9af5ac27684525bbac56d
SrCroqueta <32432079+SrCroqueta@users.noreply.github.com>
2024-05-16 03:08:05 +0200

    Add files via upload

---
+++ js/copyright_autoupdate.js
@@ -0,0 +1,12 @@
+﻿// Actualizar año actual en el footer
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtiene el elemento año por su ID
+    var yearElement = document.getElementById("copyr");
+
+    // Obtiene el año actual
+    var currentYear = new Date().getFullYear();
+
+    // Actualiza el contenido del elemento con el año actual
+    yearElement.innerHTML = `© ${currentYear}&emsp;─&emsp;<a class="copyr_enlace" href='https://github.com/SrCroqueta' target="_blank">Sr.Croqueta<a/>`;
+});
---
+++ js/showcontent.js
@@ -0,0 +1,17 @@
+﻿function mostrarContenido(containerId) {
+    ocultarTodos();
+    document.getElementById(containerId).style.display = "block";
+    document.getElementById('container-2').style.display = "none";
+}
+
+function ocultarContenido(containerId) {
+    document.getElementById(containerId).style.display = "none";
+    document.getElementById('container-2').style.display = "block";
+}
+
+function ocultarTodos() {
+    var containers = document.querySelectorAll('.container-3, .container-4, .container-5');
+    containers.forEach(function(container) {
+        container.style.display = "none";
+    });
+}
