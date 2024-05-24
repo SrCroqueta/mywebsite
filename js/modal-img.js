72000484274bd9288cd6c84e789978fd790b937c
SrCroqueta <32432079+SrCroqueta@users.noreply.github.com>
2024-05-24 18:30:03 +0200

    Add files via upload
    Añadido el script para el modal (visualizador de imágenes).

---
+++ js/modal-img.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    // Obtener el modal
+    var modal = document.getElementById("imageModal");
+
+    // Obtener la imagen y el elemento de modalImage
+    var modalImg = document.getElementById("modalImage");
+    var captionText = document.getElementById("caption");
+
+    // Función para abrir el modal con la imagen clickeada
+    window.openModal = function(imgElement) {
+        modal.style.display = "block";
+        modalImg.src = imgElement.src;
+        captionText.innerHTML = imgElement.alt;
+    };
+
+    // Obtener el elemento <span> que cierra el modal
+    var span = document.getElementById("closeModal");
+
+    // Cuando el usuario hace clic en <span> (x), cerrar el modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // Cerrar el modal cuando el usuario hace clic fuera de la imagen
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+});
