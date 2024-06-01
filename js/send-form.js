606694e26389ee78fb42b061576bf0cdf75569d5
Sr.Croqueta <32432079+SrCroqueta@users.noreply.github.com>
2024-06-01 20:19:32 +0200

    Update send-form.js
    Rediseño del script que usa el botón de envío de formulario. Ahora verifica que los campos tengan información para su envío.

--- js/send-form.js
+++ js/send-form.js
@@ -1,6 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('enviar-formulario').addEventListener('click', function(e) {
-      e.preventDefault();
-      document.getElementById('contacto').submit();
-    })
-  })
+document.getElementById('enviar-formulario').addEventListener('click', function(event) {
+    var nombre = document.getElementById('nombre').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var mensaje = document.getElementById('mensaje').value.trim();
+
+    if (nombre === '' || email === '' || mensaje === '') {
+        alert('Por favor, complete todos los campos.');
+        event.preventDefault();
+    } else {
+        document.getElementById('contacto').submit();
+    }
+});
