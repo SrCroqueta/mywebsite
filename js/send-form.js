ff7b00ca80157930eee46cd0b45805b0f6943965
Sr.Croqueta <32432079+SrCroqueta@users.noreply.github.com>
2024-06-01 20:30:47 +0200

    Update send-form.js
    Añadida una expresión regular para validad el formato de correo electrónico.

--- js/send-form.js
+++ js/send-form.js
@@ -1,12 +1,16 @@
-document.getElementById('enviar-formulario').addEventListener('click', function(event) {
-    var nombre = document.getElementById('nombre').value.trim();
-    var email = document.getElementById('email').value.trim();
-    var mensaje = document.getElementById('mensaje').value.trim();
+document.getElementById('contacto').addEventListener('submit', function(event) {
+  var nombre = document.getElementById('nombre').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var mensaje = document.getElementById('mensaje').value.trim();
 
-    if (nombre === '' || email === '' || mensaje === '') {
-        alert('Por favor, complete todos los campos.');
-        event.preventDefault();
-    } else {
-        document.getElementById('contacto').submit();
-    }
+  // Expresión regular para validar el formato de correo electrónico
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (nombre === '' || email === '' || mensaje === '') {
+      alert('Por favor, complete todos los campos.');
+      event.preventDefault();
+  } else if (!emailPattern.test(email)) {
+      alert('Por favor, introduzca un correo electrónico válido.');
+      event.preventDefault();
+  }
 });
