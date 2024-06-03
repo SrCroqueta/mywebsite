41c632881cb7c49854a079aff166a2970a787c86
Sr.Croqueta <32432079+SrCroqueta@users.noreply.github.com>
2024-06-03 17:31:32 +0200

    Update showcontent.js
    Actualización del script showcontent para el efecto de las transiciones.

--- js/showcontent.js
+++ js/showcontent.js
@@ -1,17 +1,41 @@
-﻿function mostrarContenido(containerId) {
-    ocultarTodos();
-    document.getElementById(containerId).style.display = "block";
-    document.getElementById('container-2').style.display = "none";
+function mostrarContenido(containerId) {
+    ocultarTodos(() => {
+        document.getElementById('container-2').style.display = "none";
+        var container = document.getElementById(containerId);
+        container.style.display = "block";
+        setTimeout(() => {
+            container.classList.add('show');
+        }, 10);
+    });
 }
 
 function ocultarContenido(containerId) {
-    document.getElementById(containerId).style.display = "none";
-    document.getElementById('container-2').style.display = "block";
+    var container = document.getElementById(containerId);
+    container.classList.remove('show');
+    setTimeout(() => {
+        container.style.display = "none";
+        document.getElementById('container-2').style.display = "block";
+        document.getElementById('container-2').classList.add('show');
+    }, 500); // Duración de la transición en milisegundos (coincide con la duración en CSS)
 }
 
-function ocultarTodos() {
-    var containers = document.querySelectorAll('.container-3, .container-4, .container-5, .container-6');
+function ocultarTodos(callback) {
+    var containers = document.querySelectorAll('.container-3, .container-4, .container-5, container-6');
+    var containersHidden = 0;
+    
+    if (containers.length === 0) {
+        callback();
+        return;
+    }
+
     containers.forEach(function(container) {
-        container.style.display = "none";
+        container.classList.remove('show');
+        setTimeout(function() {
+            container.style.display = "none";
+            containersHidden++;
+            if (containersHidden === containers.length && typeof callback === "function") {
+                callback();
+            }
+        }, 500); // Duración de la transición en milisegundos (coincide con la duración en CSS)
     });
 }
